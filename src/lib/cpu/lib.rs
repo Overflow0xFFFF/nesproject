f8ec7888ac47737192b8beea131599feee83d34c
Joshua Ford <joshua.ford@protonmail.com>
2021-04-11 20:17:03 -0500

    Create a rust library for emulating the CPU

---
+++ src/lib/cpu/lib.rs
@@ -0,0 +1,18 @@
+/**
+ * Library for emulating a 6502 CPU.
+ */
+pub struct CPU {
+    pub register_a: u8,
+    pub status: u8,
+    pub program_counter: u16,
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        CPU {
+            register_a: 0,
+            status: 0,
+            program_counter: 0,
+        }
+    }
+}
